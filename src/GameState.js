be72be21dc62690af39db8e4fd5e40c24e0036d6
Regendare <regendare@users.noreply.github.com>
2018-04-22 08:10:15 -0700

    Changed BG Color

--- src/GameState.js
+++ src/GameState.js
@@ -5,17 +5,22 @@ GAME.GameState = class {
     }
 
     makeGrid() {
-        const spacing = 10;
+        const spacing = 20;
         let x = 0;
         let y = 0;
 
         GAME.CTX.fillStyle = 'black';
-        for (let i = 0; i < GAME.HEIGHT / 10; i++)
-            GAME.CTX.fillRect(x,(y + (i * spacing)), GAME.WIDTH, 1)
+        for (let i = 1; i <= (GAME.HEIGHT / spacing); i++) {
+            GAME.CTX.fillRect(x,(y + (i * spacing)), GAME.WIDTH, 1);
+        }
+
+        for (let i = 1; i <= (GAME.WIDTH / spacing); i++) {
+            GAME.CTX.fillRect((x + (i * spacing)), y, 1, GAME.HEIGHT);
+        }
     }
 
     update () {
-        GAME.CTX.fillStyle = 'blue';
+        GAME.CTX.fillStyle = 'AliceBlue';
         GAME.CTX.fillRect (0, 0, GAME.WIDTH, GAME.HEIGHT);
         console.log ("This is the game state");
 
